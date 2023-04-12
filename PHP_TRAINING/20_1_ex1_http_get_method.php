e2d9d90c8f14a1776985e52f19b42f4a1dfb6750
YooSeongYeob <yoostar94@naver.com>
2023-04-12 10:25:45 +0900

    2023.04.12 get metho 통신 방법

---
+++ PHP_TRAINING/20_1_ex1_http_get_method.php
@@ -0,0 +1,39 @@
+<?php
+// http://localhost/mini_board/src/board_update.php?board_no=1
+//  여기까지 도메인//  여기까지 패스라고 부름      //
+// board_no=1은 쿼리스트리밍이라고 함
+
+// 1. Get Method로 데이터를 넘겨주는 방법 1
+//     -Query String에 키와값을 세팅해준다. 
+//     url을 직접 쳐서 해당 페이지로 이동했을 때 겟 메소드로 http 통신한다고 함
+// http://localhost/20_1_ex2_httpget.php?aaa=1 aaa가 키 1이 밸류
+// http://localhost/20_1_ex2_httpget.php?aaa=1&bbb=2  &는 앤드 쿼리스트링은 무한정 쓸 수 없음
+// 겟 방식 통신은 1~2개로 값 설정한다고 함
+//URL 통신 연결 방법
+?>  
+
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!--    1-2. form Tag를 이용하는 방법  -->
+    <form method="get" action="20_1_ex2_httpget.php">
+        <input type="text" name="test1" value="testValue1">
+        <input type="text" name="test2" value="testValue2">
+        <button type="submit">Submit</button>
+    </form> 
+    <!-- http://localhost/20_1_ex1_http_get.php?test1=testValue1 -->
+ <!--폼 태그를 메소드 겟으로 만들어서 통신 연결 -->
+ <!-- http://localhost/20_1_ex2_httpget.php?test1=testValue1&test2=testValue2 출력 됨 -->
+ <br>
+ <br>
+ <br>
+ <a href ="http://localhost/20_1_ex2_httpget.php?aaa=1">A태그</a>
+ <!-- 겟 메소드를 이용한 http 통신 -->
+</body>
+</html>
--- PHP_TRAINING/998_blackjack.php
+++ PHP_TRAINING/998_blackjack.php
@@ -8,16 +8,16 @@
 
 //-------------------함수 안 쓰고 카드 2장 뽑기-----------------//
 
-$deck = array(
-    "Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts",
-    "8 of Hearts", "9 of Hearts", "10 of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts",
-    "Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", 
-    "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", 
-    "King of Diamonds", "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", 
-    "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", 
-    "Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", 
-    "8 of Spades", "9 of Spades", "10 of Spades", "Jack of Spades", "Queen of Spades", "King of Spades"
-);
+// $deck = array(
+//     "Ace of Hearts", "2 of Hearts", "3 of Hearts", "4 of Hearts", "5 of Hearts", "6 of Hearts", "7 of Hearts",
+//     "8 of Hearts", "9 of Hearts", "10 of Hearts", "Jack of Hearts", "Queen of Hearts", "King of Hearts",
+//     "Ace of Diamonds", "2 of Diamonds", "3 of Diamonds", "4 of Diamonds", "5 of Diamonds", "6 of Diamonds", 
+//     "7 of Diamonds", "8 of Diamonds", "9 of Diamonds", "10 of Diamonds", "Jack of Diamonds", "Queen of Diamonds", 
+//     "King of Diamonds", "Ace of Clubs", "2 of Clubs", "3 of Clubs", "4 of Clubs", "5 of Clubs", "6 of Clubs", 
+//     "7 of Clubs", "8 of Clubs", "9 of Clubs", "10 of Clubs", "Jack of Clubs", "Queen of Clubs", "King of Clubs", 
+//     "Ace of Spades", "2 of Spades", "3 of Spades", "4 of Spades", "5 of Spades", "6 of Spades", "7 of Spades", 
+//     "8 of Spades", "9 of Spades", "10 of Spades", "Jack of Spades", "Queen of Spades", "King of Spades"
+// );
 
 // $card1_index = rand(0, 51);
 // $card1 = $deck[$card1_index];
@@ -85,49 +85,93 @@ public function __construct()
         foreach($cards as $card)
         {
             $num = substr($card,0,-3);
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
+            if($num == 'A')
+            {
+                $num_of_ace++;
+                $score += 11;
+            }
+        else if ( $num == 'K' || $num == 'Q' || $num == 'J' || $num =='10')
+        {
+            $score +=10;
+        }
+        else 
+        {
+            $socre += intval($num);
+        }
         }
         
+        while($score>21 && $num_of_ace>0)
+        {
+            $score-=10;
+            $num_of_ace--;
+        }
         
-    
-    
+        return $score;
     }
 
+    // 게임시작 
+    public function start_game()
+    {
+        echo"Welcome to BlackJack!\n\n";
 
+        // 카드 2장씩 받기
+        $this->user_cards[]=$this->draw_card();
+        $this->dealer_cards[]=$this->draw_card();
+        $this->user_cards[]=$this->draw_card();
+        $this->dealer_cards[]=$this->draw_card();
 
+        $user_score=$this->calculate_score($this->user_cards);
+        $dealer_score=$this->calculate_score($this->dealer_cards);
+        
+        // 유저 블랙잭
+        if($user_score ==21)
+        {
+            echo "user has BlackJack!\nUser Cards::".implode(",",$this->user_cards)."\n\nUser Wins!";
+            exit;
+        }
+        
+        // 딜러 블랙잭
+        if($dealer_score == 21)
+        {
+            echo "Dealer has BlackJack!\nUser Cards:".implode(",",$this->user_cards)."\n\nDealer
+            Wins!";
+                exit;
+        }
 
+        // 유저가 버스트인 경우
+        if($user_score>21)
+        {
+            echo"user is busted!\nUer Cards:".implode(",",$this->user_cards)."\n\nUser Wins!";
+            exit;
+        }
 
+        //딜러가 버스트인 경우
+        if($dealer_score>21)
+        {
+            echo"Dealer is busted!\nUser Cards:".implode(",",$this->user_cards)."\n\nUser Wins!";
+            exit;
+        }
 
+        // 카드합 비교
+        if($user_score>$dealer_score)
+        {
+            echo "User score>Dealer score!\nUser Cards:".implode(",",$this->user_cards)."\n\nUser Wins!"; 
+        }
+        else if($user_score<$dealer_score)
+        {
+            echo"User score<Dealer score!\nUser Cards:".implode(",",$this>user_cards)."\n\nDealer Wins!";
+        }
+        else
+        {
+            echo "it's a tie!\n";
+        }
+        exit;
+        }
+    };
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-}
-
+    $obj_bj = new BlackJack();
+    $obj_bj->start_game();
+    
 
 // class blakjack {
 //     public $deck;
